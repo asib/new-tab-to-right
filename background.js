2cd0ef631146586f57ec4475e7df7fd14aa7b4a6
Jacob Fenton <jacob@fly.io>
2024-01-03 13:08:59 +0000

    Add shortcuts for moving the active tab right/left.

--- background.js
+++ background.js
@@ -3,5 +3,13 @@ chrome.commands.onCommand.addListener(function(command) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.create({index: tabs[0].index + 1});
         });
+    } else if (command == "move-tab-right") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.move(tabs[0].id, {index: tabs[0].index + 1});
+        });
+    } else if (command == "move-tab-left") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.move(tabs[0].id, {index: tabs[0].index - 1});
+        });
     }
 });

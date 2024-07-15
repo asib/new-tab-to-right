b84ec82623a61280d0444e4be63b05945ac428a7
Jacob Fenton <jacob@fly.io>
2024-07-15 16:58:48 +0100

    Allow wraparound to the right.

--- background.js
+++ background.js
@@ -4,11 +4,16 @@ chrome.commands.onCommand.addListener(function(command) {
             chrome.tabs.create({index: tabs[0].index + 1});
         });
     } else if (command == "move-tab-right") {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.move(tabs[0].id, {index: tabs[0].index + 1});
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+            chrome.tabs.query({lastFocusedWindow: true}, function (allTabs) {
+                // If the tab is already the rightmost tab, wrap around to leftmost.
+                const newIndex = tabs[0].index === (allTabs.length - 1) ? 0 : tabs[0].index + 1;
+                chrome.tabs.move(tabs[0].id, {index: newIndex});
+            });
         });
     } else if (command == "move-tab-left") {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+            // No special casing needed here - setting index to -1 will wrap the tab round correctly.
             chrome.tabs.move(tabs[0].id, {index: tabs[0].index - 1});
         });
     }
